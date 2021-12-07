494d34e87491574ce57d8d48fa66dadc3d6dbaea
matterpreter <matt@matterpreter.com>
2021-12-07 07:50:07 -0500

    Code cleanup and output formatting fixes

--- FindETWProviderImage/Program.cs
+++ FindETWProviderImage/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -11,7 +10,8 @@ namespace FindETWProviderImage
 {
     internal class Program
     {
-        private static readonly string Usage = "FindETWProviderImage.exe \"{provider-guid}\" \"<search_path>\"";
+        private static readonly string Usage = 
+            "FindETWProviderImage.exe \"{provider-guid}\" \"<search_path>\"";
 
         private static string TargetGuid;
         private static byte[] ProviderGuidBytes;
@@ -20,43 +20,54 @@ namespace FindETWProviderImage
 
         static void Main(string[] args)
         {
-            if (args.Length < 2)
+            try
             {
-                Console.WriteLine(Usage);
-                return;
+                if (args.Length < 2)
+                {
+                    Console.WriteLine(Usage);
+                    return;
+                }
+                if (!Guid.TryParse(args[0], out _))
+                {
+                    throw new ArgumentException("The GUID provided does not appear to be valid");
+                }
+                if (!Directory.Exists(args[1]) && !File.Exists(args[1]))
+                {
+                    throw new FileNotFoundException("The target file or directory does not exist");
+                }
+
+                Stopwatch sw = Stopwatch.StartNew();
+
+                TargetGuid = args[0];
+                ProviderGuidBytes = Guid.Parse(args[0]).ToByteArray();
+                string SearchRoot = args[1];
+
+                // Check if the search target is a directory
+                if ((File.GetAttributes(SearchRoot) & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    // Create a list of files to check by recursively searching the target directory
+                    TargetFiles = GetAllFiles(SearchRoot);
+                    Console.WriteLine($"Searching {TargetFiles.Count} files for {TargetGuid}...\n");
+
+                    // Iterate over the collection of files
+                    Parallel.ForEach(TargetFiles, file => ParseFile(file, ProviderGuidBytes));
+                }
+                else
+                {
+                    // Target is a single file
+                    ParseFile(SearchRoot, ProviderGuidBytes);
+                }
+
+                Console.WriteLine($"Total References: {TotalReferences}");
+                sw.Stop();
+                Console.WriteLine($"Time Elapsed: {sw.ElapsedMilliseconds / 1000.0000} seconds");
             }
-            if (!Guid.TryParse(args[0], out _))
+            catch (Exception ex)
             {
-                throw new ArgumentException("The GUID provided does not appear to be valid");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
             }
-            if (!Directory.Exists(args[1]) && !File.Exists(args[1]))
-            {
-                throw new FileNotFoundException("The target file or directory does not exist");
-            }
-
-            Stopwatch sw = Stopwatch.StartNew();
-
-            TargetGuid = args[0];
-            ProviderGuidBytes = Guid.Parse(args[0]).ToByteArray();
-            string SearchRoot = args[1];
-
-            // Check if the search target is a directory
-            if ((File.GetAttributes(SearchRoot) & FileAttributes.Directory) == FileAttributes.Directory)
-            {
-                // Build the list of files
-                TargetFiles = GetAllFiles(SearchRoot);
-                Console.WriteLine($"Searching {TargetFiles.Count} files for {TargetGuid}...");
-
-                Parallel.ForEach(TargetFiles, file => ParseFile(file, ProviderGuidBytes));
-            }
-            else
-            {
-                ParseFile(SearchRoot, ProviderGuidBytes);                
-            }
-
-            Console.WriteLine($"\nTotal References: {TotalReferences}");
-            sw.Stop();
-            Console.WriteLine($"Time Elapsed: {sw.ElapsedMilliseconds / 1000.0000} seconds");
+            
         }
 
         static void ParseFile(string FilePath, byte[] ProviderGuid)
@@ -67,8 +78,7 @@ namespace FindETWProviderImage
 
             if (Offsets.Count > 0)
             {
-                Console.WriteLine($"\nTarget File: {FilePath}\n" +
-                            $"GUID: {TargetGuid}\n" +
+                Console.WriteLine($"Target File: {FilePath}\n" +
                             $"Found {Offsets.Count} references:");
                 foreach (var Offset in Offsets)
                 {
@@ -77,7 +87,7 @@ namespace FindETWProviderImage
                         Offset,
                         OffsetToRVA(FileBytes, Offset));
                 }
-
+                Console.WriteLine();
                 TotalReferences += Offsets.Count;
             }
         }
@@ -143,7 +153,8 @@ namespace FindETWProviderImage
             var SectionHeaders = reader.PEHeaders.SectionHeaders;
             foreach (SectionHeader Header in SectionHeaders)
             {
-                if (Offset > Header.VirtualAddress && Offset < Header.VirtualAddress + Header.VirtualSize)
+                if (Offset > Header.VirtualAddress && 
+                    Offset < Header.VirtualAddress + Header.VirtualSize)
                 {
                     return Offset + Header.VirtualAddress - Header.PointerToRawData;
                 }
@@ -160,8 +171,9 @@ namespace FindETWProviderImage
                 RecurseSubdirectories = true,
             };
 
-            return Directory.EnumerateFiles(SearchDirectory, "*.*", Options)
-                .Where(s => s.EndsWith(".dll") || s.EndsWith(".exe") || s.EndsWith(".sys")).ToHashSet<string>();
+            return Directory.EnumerateFiles(SearchDirectory, "*.*", Options).Where(
+                s => s.EndsWith(".dll") || s.EndsWith(".exe") || s.EndsWith(".sys"))
+                .ToHashSet();
         }
     }
 }
